f15b62502197393245008adf649cb8c9c86da8bc
Ezequiel Saporito <ezequiel.saporito@gmail.com>
2024-01-21 19:57:10 -0300

    .

--- front/src/pages/PreguntasFrecuentesPage.js
+++ front/src/pages/PreguntasFrecuentesPage.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { useState, useEffect } from 'react';
 
 import Pregunta from "../components/Pregunta.js";
 
 const PreguntasFrecuentesPage = (props) => {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response)=> response.json())
-            .then((data)=>setData(data));
-    }, []);
     return (
         <div class="preguntas">
             <Pregunta pregunta={"¿Realizan envios a todo el pais?"} respuesta={"Si."}/>
-            {data?.map((user)=>(
-                <li key={user.id}>{user.name}</li>
-            ))};
         </div>
     );
 }
