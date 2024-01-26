b067bade9cf8742597a841036cca68f3bba626fb
ec13sapo <142032331+ec13sapo@users.noreply.github.com>
2024-01-25 22:35:30 -0300

    Delete back directory

--- back/routes/admin/mainAdmin.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('admin/mainAdmin', { layout: 'admin/layout', usuario: req.session.nombre});
-});
-
-module.exports = router;
